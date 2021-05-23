0a80ad524d605cf5daebb1febc0b508f7132104b
FreezeMandu <sewolminsuk03@gmail.com>
2021-05-23 11:44:49 +0900

    22

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,4 @@ Blocks.blastDrill.hasShadow = false
 Blocks.laserDrill.hasShadow = false
 Blocks.mechanicalDrill.hasShadow = false
 Blocks.pneumaticDrill.hasShadow = false
+Blocks.copperWallLarge.hasShadow = false 
