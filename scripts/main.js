3ad1ed131ad2d642cc20f8e67c46801c16604385
FreezeMandu <sewolminsuk03@gmail.com>
2021-07-06 00:32:27 +0900

    js수정

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,12 @@
 Blocks.blastDrill.hasShadow = false
 Blocks.laserDrill.hasShadow = false
+
 Blocks.mechanicalDrill.hasShadow = false
 Blocks.pneumaticDrill.hasShadow = false
 Blocks.copperWallLarge.hasShadow = false 
 Blocks.thoriumWallLarge.hasShadow = false
 Blocks.surgeWallLarge.hasShadow = false
+Blocks.plastaniumWallLarge.hasShadow = false
+Blocks.phaseWallLarge.hasShadow = false
+
 Blocks.thermalGenerator.hasShadow =  false
