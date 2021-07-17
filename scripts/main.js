e4ae38cabb65976a0fb386ec316a4dcb4435adb0
BetamindyWeb <boogye43401@gmail.com>
2021-07-17 23:35:47 +0900

    :D

--- scripts/main.js
+++ scripts/main.js
@@ -2,31 +2,4 @@ Vars.content.blocks().each(b => {
 if(((b instanceof Wall) || (b instanceof BaseTurret) || (b instanceof Drill))) b.hasShadow = false;
 });
 
-/* Blocks.blastDrill.hasShadow = false //Drills
-Blocks.laserDrill.hasShadow = false
-Blocks.mechanicalDrill.hasShadow = false
-Blocks.pneumaticDrill.hasShadow = false
-
-Blocks.copperWallLarge.hasShadow = false //Large Walls
-Blocks.thoriumWallLarge.hasShadow = false
-Blocks.surgeWallLarge.hasShadow = false
-Blocks.plastaniumWallLarge.hasShadow = false
-Blocks.phaseWallLarge.hasShadow = false
-Blocks.titaniumWallLarge.hasShadow = false
-
 Blocks.thermalGenerator.hasShadow =  false
-
-Blocks.scatter.hasShadow = false //Terrets, size: 2
-Blocks.salvo.hasShadow = false
-Blocks.lancer.hasShadow = false
-Blocks.swarmer.hasShadow = false
-Blocks.wave.hasShadow = false
-Blocks.segment.hasShadow = false
-Blocks.parallax.hasShadow = false
-Blocks.fuse.hasShadow = false //size: 3
-Blocks.ripple.hasShadow = false
-Blocks.cyclone.hasShadow = false
-Blocks.tsunami.hasShadow = false
-Blocks.foreshadow.hasShadow = false //size: 4
-Blocks.meltdown.hasShadow = false
-Blocks.spectre.hasShadow = false */
