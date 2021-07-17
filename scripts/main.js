58508d24c5e6869cace7f7b540fa6b6e67200ccd
BetamindyWeb <boogye43401@gmail.com>
2021-07-17 23:22:05 +0900

    123

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 Vars.content.blocks.each(b => {
-    if(b.update) b.hasShadow = false;
-    });
+if(b.update) b.hasShadow = false;
+});
 
 /* Blocks.blastDrill.hasShadow = false //Drills
 Blocks.laserDrill.hasShadow = false
