1a01a8697ac47d0debccfcac7c0ab279cef02e61
BetamindyWeb <boogye43401@gmail.com>
2021-07-17 23:08:29 +0900

    js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
-Blocks.blastDrill.hasShadow = false
+Blocks.blastDrill.hasShadow = false //Drills
 Blocks.laserDrill.hasShadow = false
-
 Blocks.mechanicalDrill.hasShadow = false
 Blocks.pneumaticDrill.hasShadow = false
-Blocks.copperWallLarge.hasShadow = false 
+
+Blocks.copperWallLarge.hasShadow = false //Large Walls
 Blocks.thoriumWallLarge.hasShadow = false
 Blocks.surgeWallLarge.hasShadow = false
 Blocks.plastaniumWallLarge.hasShadow = false
@@ -11,3 +11,18 @@ Blocks.phaseWallLarge.hasShadow = false
 Blocks.titaniumWallLarge.hasShadow = false
 
 Blocks.thermalGenerator.hasShadow =  false
+
+Blocks.scatter.hasShadow = false //Terrets, size: 2
+Blocks.salvo.hasShadow = false
+Blocks.lancer.hasShadow = false
+Blocks.wave.hasShadow = false
+Blocks.swamer.hasShadow = false
+Blocks.segment.hasShadow = false
+Blocks.parallax.hasShadow = false
+Blocks.fuse.hasShadow = false //size: 3
+Blocks.ripple.hasShadow = false
+Blocks.cyclone.hasShadow = false
+Blocks.tsunami.hasShadow = false
+Blocks.foreshadow.hasShadow = false //size: 4
+Blocks.meltdown.hasShadow = false
+Blocks.spectre.hasShadow = false
