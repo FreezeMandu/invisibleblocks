428f4795a1105e3a02975695b7694549ff74f005
BetamindyWeb <boogye43401@gmail.com>
2021-07-17 23:34:08 +0900

    :D

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 Vars.content.blocks().each(b => {
-if(((b instanceof Wall) || (b instanceof BaseTurret))) b.hasShadow = false;
+if(((b instanceof Wall) || (b instanceof BaseTurret) || (b instanceof Drill))) b.hasShadow = false;
 });
 
 /* Blocks.blastDrill.hasShadow = false //Drills
